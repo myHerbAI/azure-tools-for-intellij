13b44271b82b9676da09e74bb528e64e6343ebdb
Miller Wang <wangmi@microsoft.com>
2023-07-28 14:04:27 +0800

    fix context issue about getting started.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-applicationinsights/src/main/java/com/microsoft/azure/toolkit/intellij/applicationinsights/task/CreateApplicationInsightsResourceConnectionTask.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-applicationinsights/src/main/java/com/microsoft/azure/toolkit/intellij/applicationinsights/task/CreateApplicationInsightsResourceConnectionTask.java
@@ -41,21 +41,19 @@ public class CreateApplicationInsightsResourceConnectionTask implements Task {
         final Module module = getModule();
         final Resource consumer = getModuleConsumer(module);
         final AzureModule azureModule = AzureModule.from(module);
-        AzureTaskManager.getInstance().runAndWait(() -> {
+        final AzureTaskManager tm = AzureTaskManager.getInstance();
+        tm.runAndWait(() -> tm.write(() -> {
             final Profile profile = azureModule.initializeWithDefaultProfileIfNot();
-            if (Objects.isNull(profile)) {
-                return;
-            }
             final Connection<?, ?> connection = ConnectionManager.getDefinitionOrDefault(resource.getDefinition(),
-                    consumer.getDefinition()).define(resource, consumer);
+                consumer.getDefinition()).define(resource, consumer);
             if (connection.validate(this.project)) {
                 profile.createOrUpdateConnection(connection);
                 profile.save();
                 final String message = String.format("The connection between %s and %s has been successfully created.",
-                        resource.getName(), consumer.getName());
+                    resource.getName(), consumer.getName());
                 AzureMessager.getMessager().success(message);
             }
-        });
+        }));
     }
 
     @Nonnull
@@ -66,7 +64,7 @@ public class CreateApplicationInsightsResourceConnectionTask implements Task {
 
     private Resource<ApplicationInsight> getResource() {
         final String applicationInsightsId = (String) Objects.requireNonNull(context.getParameter("applicationInsightsId"),
-                "`applicationInsightsId` should not be null to create a resource connection");
+            "`applicationInsightsId` should not be null to create a resource connection");
         final ApplicationInsight applicationInsight = Azure.az(AzureApplicationInsights.class).getById(applicationInsightsId);
         return ApplicationInsightsResourceDefinition.INSTANCE.define(applicationInsight);
     }
