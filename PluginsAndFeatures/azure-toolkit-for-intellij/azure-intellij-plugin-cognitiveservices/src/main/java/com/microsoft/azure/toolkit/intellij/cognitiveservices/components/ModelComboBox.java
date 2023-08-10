6424cfe5e091f183d7220177a103799373cc2a1f
Flanker-MSFT <flanker-msft@LiudeMac-mini.local>
2023-08-10 15:05:55 +0800

    Add components for cognitive services creation dialog

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cognitiveservices/src/main/java/com/microsoft/azure/toolkit/intellij/cognitiveservices/components/ModelComboBox.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij.cognitiveservices.components;
+
+import com.intellij.ui.components.fields.ExtendableTextComponent;
+import com.microsoft.azure.toolkit.intellij.common.AzureComboBox;
+import com.microsoft.azure.toolkit.lib.cognitiveservices.CognitiveAccount;
+import com.microsoft.azure.toolkit.lib.cognitiveservices.model.AccountModel;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+public class ModelComboBox extends AzureComboBox<AccountModel> {
+    @Nonnull
+    private final CognitiveAccount account;
+
+    public ModelComboBox(@Nonnull final CognitiveAccount account) {
+        super(true);
+        this.account = account;
+        this.setItemsLoader(account::listModels);
+    }
+
+    @Nonnull
+    @Override
+    protected List<ExtendableTextComponent.Extension> getExtensions() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected String getItemText(final Object item) {
+        if (item instanceof AccountModel) {
+            return String.format("%s (version: %s)", ((AccountModel) item).getName(), ((AccountModel) item).getVersion());
+        }
+        return super.getItemText(item);
+    }
+}
