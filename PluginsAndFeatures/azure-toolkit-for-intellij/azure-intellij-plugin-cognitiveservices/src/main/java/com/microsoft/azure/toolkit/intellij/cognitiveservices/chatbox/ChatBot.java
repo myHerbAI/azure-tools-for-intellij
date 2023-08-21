5308955d84385ead0eadbf8125081b2edbb572d3
Miller Wang <wangmi@microsoft.com>
2023-08-21 22:55:11 +0800

    set configuration

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cognitiveservices/src/main/java/com/microsoft/azure/toolkit/intellij/cognitiveservices/chatbox/ChatBot.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cognitiveservices/src/main/java/com/microsoft/azure/toolkit/intellij/cognitiveservices/chatbox/ChatBot.java
@@ -8,6 +8,7 @@ import com.azure.ai.openai.models.ChatCompletionsOptions;
 import com.azure.ai.openai.models.ChatMessage;
 import com.azure.ai.openai.models.ChatRole;
 import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.Configuration;
 import com.microsoft.azure.toolkit.lib.cognitiveservices.CognitiveAccount;
 import com.microsoft.azure.toolkit.lib.cognitiveservices.CognitiveDeployment;
 import lombok.Getter;
@@ -18,39 +19,52 @@ import java.util.Stack;
 
 public class ChatBot {
     private final Stack<ChatMessage> chatMessages = new Stack<>();
-    private final OpenAIClient client;
     @Getter
     private final CognitiveDeployment deployment;
-    @Nonnull
+
     private String systemMessage;
+    private Configuration configuration;
+    private OpenAIClient client;
 
     public ChatBot(final CognitiveDeployment deployment) {
         this.deployment = deployment;
-        final CognitiveAccount account = deployment.getParent();
+        this.refreshClient();
+    }
+
+    private OpenAIClient refreshClient() {
+        final CognitiveAccount account = this.deployment.getParent();
         final String endpoint = account.getEndpoint();
         final String primaryKey = Objects.requireNonNull(account.getPrimaryKey());
         this.client = new OpenAIClientBuilder()
             .endpoint(endpoint)
+            .configuration(this.configuration)
             .credential(new AzureKeyCredential(primaryKey))
             .buildClient();
+        return this.client;
     }
 
     public void setSystemMessage(@Nonnull final String systemMessage) {
         this.systemMessage = systemMessage;
-        chatMessages.push(new ChatMessage(ChatRole.SYSTEM, systemMessage));
+        this.chatMessages.clear();
+        this.chatMessages.push(new ChatMessage(ChatRole.SYSTEM, systemMessage));
+    }
+
+    public void setConfiguration(@Nonnull final Configuration configuration) {
+        this.configuration = configuration;
+        this.refreshClient();
     }
 
     public ChatChoice send(String message) {
-        chatMessages.push(new ChatMessage(ChatRole.USER, message));
+        this.chatMessages.push(new ChatMessage(ChatRole.USER, message));
         final String deploymentName = this.deployment.getName();
-        final ChatCompletions chatCompletions = client.getChatCompletions(deploymentName, new ChatCompletionsOptions(chatMessages));
+        final ChatCompletions chatCompletions = this.client.getChatCompletions(deploymentName, new ChatCompletionsOptions(this.chatMessages));
         final ChatChoice chatChoice = chatCompletions.getChoices().get(0);
-        chatMessages.push(chatChoice.getMessage());
+        this.chatMessages.push(chatChoice.getMessage());
         return chatChoice;
     }
 
     public void reset() {
-        chatMessages.clear();
-        chatMessages.push(new ChatMessage(ChatRole.SYSTEM, systemMessage));
+        this.chatMessages.clear();
+        this.chatMessages.push(new ChatMessage(ChatRole.SYSTEM, this.systemMessage));
     }
 }
