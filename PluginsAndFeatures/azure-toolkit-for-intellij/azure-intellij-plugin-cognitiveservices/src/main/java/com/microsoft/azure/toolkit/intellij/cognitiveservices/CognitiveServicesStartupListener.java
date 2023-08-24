ec5b85a803dc9b2710d813bf372e265d9b6a3815
Miller Wang <wangmi@microsoft.com>
2023-08-24 10:41:15 +0800

    fix notification message about try ai playground.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cognitiveservices/src/main/java/com/microsoft/azure/toolkit/intellij/cognitiveservices/CognitiveServicesStartupListener.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cognitiveservices/src/main/java/com/microsoft/azure/toolkit/intellij/cognitiveservices/CognitiveServicesStartupListener.java
@@ -25,13 +25,11 @@ public class CognitiveServicesStartupListener implements ProjectActivity {
     public static final AzureString TRY_OPENAI_MESSAGE = AzureString.format("%s is supported now. " +
         "You can use Azure OpenAI to build your own \"%s\". " +
         "<a href='https://docs.microsoft.com/en-us/azure/openai/'>learn more</a> about Azure OpenAI.", "Azure OpenAI", "ChatGPT");
-    public static final AzureString TRY_PLAYGROUND_MESSAGE = AzureString.format("%s model based deployments detected in your Cognitive Service accounts. " +
-        "You can try your own \"%s\" in playground.", "GPT*", "ChatGPT");
 
     @Override
     public Object execute(@Nonnull Project project, @Nonnull Continuation<? super Unit> continuation) {
         final String tryOpenAIId = TRY_OPENAI.getId();
-        final String tryPlaygroundId = "user/cognitiveservices.try_playground";
+        final String tryPlaygroundId = TRY_PLAYGROUND.getId();
         if (BooleanUtils.isNotTrue(IntellijStore.getInstance().getState().getSuppressedActions().get(tryOpenAIId))) {
             final Action<Project> tryOpenAI = AzureActionManager.getInstance().getAction(TRY_OPENAI).bind(project);
             final Action<Object> dismiss = new Action<>(Action.Id.of("user/common.never_show_again"))
@@ -41,28 +39,28 @@ public class CognitiveServicesStartupListener implements ProjectActivity {
             AzureMessager.getMessager().info(TRY_OPENAI_MESSAGE, tryOpenAI, dismiss);
         }
         if (BooleanUtils.isNotTrue(IntellijStore.getInstance().getState().getSuppressedActions().get(tryPlaygroundId))) {
-            AzureEventBus.once("account.subscription_changed.account", (_a, _b) -> {
-                Azure.az(AzureCognitiveServices.class).list().stream()
-                    .flatMap(m -> m.accounts().list().stream())
-                    .flatMap(a -> a.deployments().list().stream())
-                    //TODO: filter gpt deployments
-                    .findFirst().ifPresent(d -> {
-                        final Action<CognitiveDeployment> tryPlayGround =
-                            new Action<>(TRY_PLAYGROUND)
-                                .withLabel("Open in Playground")
-                                .withSource(d)
-                                .withHandler((_d, e) -> {
-                                    AzureActionManager.getInstance().getAction(OPEN_DEPLOYMENT_IN_PLAYGROUND).handle(d, e);
-                                    IntellijStore.getInstance().getState().getSuppressedActions().put(tryPlaygroundId, true);
-                                })
-                                .withAuthRequired(true);
-                        final Action<Object> dismiss = new Action<>(Action.Id.of("user/common.never_show_again"))
-                            .withLabel("Don't show again")
-                            .withHandler((e) -> IntellijStore.getInstance().getState().getSuppressedActions().put(tryPlaygroundId, true))
-                            .withAuthRequired(false);
-                        AzureMessager.getMessager().info(TRY_PLAYGROUND_MESSAGE, tryPlayGround, dismiss);
-                    });
-            });
+            AzureEventBus.once("account.subscription_changed.account", (_a, _b) -> Azure.az(AzureCognitiveServices.class).list().stream()
+                .flatMap(m -> m.accounts().list().stream())
+                .flatMap(a -> a.deployments().list().stream())
+                .filter(d -> d.getModel().isGPTModel())
+                .findFirst().ifPresent(d -> {
+                    final Action<CognitiveDeployment> tryPlayGround =
+                        new Action<>(TRY_PLAYGROUND)
+                            .withLabel("Open in AI Playground")
+                            .withSource(d)
+                            .withHandler((_d, e) -> {
+                                AzureActionManager.getInstance().getAction(OPEN_DEPLOYMENT_IN_PLAYGROUND).handle(d, e);
+                                IntellijStore.getInstance().getState().getSuppressedActions().put(tryPlaygroundId, true);
+                            })
+                            .withAuthRequired(true);
+                    final Action<Object> dismiss = new Action<>(Action.Id.of("user/common.never_show_again"))
+                        .withLabel("Don't show again")
+                        .withHandler((e) -> IntellijStore.getInstance().getState().getSuppressedActions().put(tryPlaygroundId, true))
+                        .withAuthRequired(false);
+                    final AzureString msg = AzureString.format("GPT* model based deployment (%s) detected in your Cognitive Service account (%s). " +
+                        "You can try your own \"%s\" in playground.", d.getName(), d.getParent().getName(), "ChatGPT");
+                    AzureMessager.getMessager().info(msg, tryPlayGround, dismiss);
+                }));
         }
         return null;
     }
