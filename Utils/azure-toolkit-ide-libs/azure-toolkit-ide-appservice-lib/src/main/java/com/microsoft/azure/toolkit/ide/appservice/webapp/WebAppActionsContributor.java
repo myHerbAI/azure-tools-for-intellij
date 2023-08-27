336905f7f40029f8a28a47138615f82ebdb1ac28
Flanker-MSFT <flanker-msft@Bumblebee.redmond.corp.microsoft.com>
2023-08-27 20:38:11 +0800

    Rename Open Getting Start to Getting Started to keep consistent with previous action

--- Utils/azure-toolkit-ide-libs/azure-toolkit-ide-appservice-lib/src/main/java/com/microsoft/azure/toolkit/ide/appservice/webapp/WebAppActionsContributor.java
+++ Utils/azure-toolkit-ide-libs/azure-toolkit-ide-appservice-lib/src/main/java/com/microsoft/azure/toolkit/ide/appservice/webapp/WebAppActionsContributor.java
@@ -36,7 +36,7 @@ public class WebAppActionsContributor implements IActionsContributor {
         final ActionGroup serviceActionGroup = new ActionGroup(
             ResourceCommonActionsContributor.REFRESH,
             "---",
-            ResourceCommonActionsContributor.OPEN_GETTING_START,
+            ResourceCommonActionsContributor.GETTING_STARTED,
             ResourceCommonActionsContributor.OPEN_AZURE_REFERENCE_BOOK,
             "---",
             ResourceCommonActionsContributor.CREATE
