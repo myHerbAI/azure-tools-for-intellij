de5994b029e6ce85abd7a7ef88264fc6f947597a
Miller Wang <wangmi@microsoft.com>
2023-11-01 19:56:04 +0800

    make the whole key part(properties file) as code completion prefix to fix AB#2119080

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-lib-java/src/main/java/com/microsoft/azure/toolkit/intellij/connector/code/spring/PropertiesKeyCompletionProvider.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-lib-java/src/main/java/com/microsoft/azure/toolkit/intellij/connector/code/spring/PropertiesKeyCompletionProvider.java
@@ -37,6 +37,11 @@ public class PropertiesKeyCompletionProvider extends CompletionProvider<Completi
 
     @Override
     protected void addCompletions(@Nonnull CompletionParameters parameters, @Nonnull ProcessingContext context, @Nonnull CompletionResultSet result) {
+        final String originalPrefix = result.getPrefixMatcher().getPrefix();
+        final String prefix = StringUtils.substringBefore(parameters.getPosition().getParent().getLastChild().getText(), CompletionUtil.DUMMY_IDENTIFIER_TRIMMED);
+        if (!StringUtils.equals(originalPrefix, prefix)) {
+            result = result.withPrefixMatcher(prefix);
+        }
         final PsiFile file = parameters.getOriginalFile();
         definitions.stream().flatMap(definition ->
             definition.getSpringProperties().stream().filter(p -> !p.getKey().trim().startsWith("#")).map(p -> LookupElementBuilder
