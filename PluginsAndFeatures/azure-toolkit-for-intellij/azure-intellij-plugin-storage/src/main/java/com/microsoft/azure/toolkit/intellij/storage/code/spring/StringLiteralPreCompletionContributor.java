0b8a02e5ba88f0f1a4bbee1ce98587bb71918851
Miller Wang <wangmi@microsoft.com>
2023-11-03 17:17:30 +0800

    change order of completion contributor so that `Connect Azure Storage account...` completion item show first when lazy loading storage accounts

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-storage/src/main/java/com/microsoft/azure/toolkit/intellij/storage/code/spring/StringLiteralPreCompletionContributor.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-storage/src/main/java/com/microsoft/azure/toolkit/intellij/storage/code/spring/StringLiteralPreCompletionContributor.java
@@ -61,7 +61,6 @@ public class StringLiteralPreCompletionContributor extends CompletionContributor
                             }
                         }));
                     }
-                    result.stopHere();
                 }
             }
         });
