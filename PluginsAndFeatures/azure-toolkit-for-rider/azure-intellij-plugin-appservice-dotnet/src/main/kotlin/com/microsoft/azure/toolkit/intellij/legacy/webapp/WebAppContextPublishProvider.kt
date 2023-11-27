1ec0409fe221479f3d8c746e80aa2e33bc3b9b39
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2023-11-27 14:41:36 +0100

    Add WebAppContextPublishProvider

---
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/webapp/WebAppContextPublishProvider.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the MIT license.
+ */
+
+package com.microsoft.azure.toolkit.intellij.legacy.webapp
+
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.ConfigurationTypeUtil
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
+import com.jetbrains.rider.model.PublishableProjectModel
+import com.jetbrains.rider.model.publishableProjectsModel
+import com.jetbrains.rider.projectView.solution
+import com.jetbrains.rider.projectView.workspace.ProjectModelEntity
+import com.jetbrains.rider.run.configurations.publishing.RiderContextPublishProvider
+import com.microsoft.azure.toolkit.ide.common.icon.AzureIcons
+import com.microsoft.azure.toolkit.intellij.common.IntelliJAzureIcons
+import com.microsoft.azure.toolkit.intellij.legacy.webapp.runner.webappconfig.RiderWebAppConfiguration
+import com.microsoft.azure.toolkit.intellij.legacy.webapp.runner.webappconfig.WebAppConfigurationType
+import javax.swing.Icon
+
+class WebAppContextPublishProvider : RiderContextPublishProvider {
+    override val icon: Icon
+        get() = IntelliJAzureIcons.getIcon(AzureIcons.Common.AZURE)
+    override val name: String
+        get() = "Publish to Azure"
+
+    override fun getConfigurationForNode(
+        project: Project,
+        entity: ProjectModelEntity
+    ): Pair<RunConfiguration, ConfigurationFactory> {
+        val projectData = RiderContextPublishProvider.getProjectDataRecursive(project, entity)
+            ?: error("Unexpected project node type. Cannot get project data for entity ${entity.url}")
+
+        val factory =
+            ConfigurationTypeUtil.findConfigurationType(WebAppConfigurationType::class.java).configurationFactories.single()
+        val configuration =
+            RiderWebAppConfiguration(project, factory, "Publish ${projectData.value.projectName} to Azure")
+
+        configuration.saveProject(projectData.value)
+
+        return Pair(configuration, factory)
+    }
+
+    private fun isProjectPublishable(projectData: PublishableProjectModel?) =
+        projectData != null && (projectData.isWeb && (projectData.isDotNetCore || SystemInfo.isWindows))
+
+    override fun isAvailable(project: Project, entity: ProjectModelEntity): Boolean {
+        val projectData = RiderContextPublishProvider.getProjectData(project, entity)
+        return isProjectPublishable(projectData?.value)
+    }
+
+    override fun solutionHasAnyPublishableProjects(project: Project) =
+        project.solution.publishableProjectsModel.publishableProjects.entries.any { isProjectPublishable(it.value) }
+}
