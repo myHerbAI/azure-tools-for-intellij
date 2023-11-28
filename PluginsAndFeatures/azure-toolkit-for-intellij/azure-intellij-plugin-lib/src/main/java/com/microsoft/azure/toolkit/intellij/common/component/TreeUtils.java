8851c6d7b7143d5de0cafd8e58b5df551811e521
wangmi <wangmi@microsoft.com>
2023-11-28 16:51:40 +0800

    select the latest located ancestor when the target is not found.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/component/TreeUtils.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/component/TreeUtils.java
@@ -337,7 +337,10 @@ public class TreeUtils {
             }
         }).onSuccess(path -> {
             tree.getModel().removeTreeModelListener(listener);
-            TreeUtil.selectPath(tree, path, true);
+        }).onProcessed(path -> {
+            if (Objects.nonNull(checkpoint.get())) {
+                TreeUtil.selectPath(tree, checkpoint.get(), true);
+            }
         });
     }
 }
