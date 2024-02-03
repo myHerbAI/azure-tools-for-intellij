eb7c25357e928060eb0c1761eb1f368f915dc055
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-02-03 21:44:18 +0100

    Add Azurite before run task

---
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/azurite/AzuriteBeforeRunTaskProvider.kt
@@ -0,0 +1,42 @@
+package com.microsoft.azure.toolkit.intellij.azurite
+
+import com.intellij.execution.BeforeRunTask
+import com.intellij.execution.BeforeRunTaskProvider
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.util.Key
+import com.microsoft.azure.toolkit.ide.common.icon.AzureIcons
+import com.microsoft.azure.toolkit.intellij.azurite.services.AzuriteService
+import com.microsoft.azure.toolkit.intellij.common.IntelliJAzureIcons
+import javax.swing.Icon
+
+class AzuriteBeforeRunTask : BeforeRunTask<AzuriteBeforeRunTask>(AzuriteBeforeRunTaskProvider.ID)
+
+class AzuriteBeforeRunTaskProvider : BeforeRunTaskProvider<AzuriteBeforeRunTask>() {
+    companion object {
+        val ID = Key.create<AzuriteBeforeRunTask>("RunAzuriteTask")
+    }
+
+    override fun getId() = ID
+
+    override fun getName() = "Start Azurite"
+
+    override fun getIcon(): Icon = IntelliJAzureIcons.getIcon(AzureIcons.StorageAccount.AZURITE)
+
+    override fun createTask(runConfiguration: RunConfiguration): AzuriteBeforeRunTask {
+        val task = AzuriteBeforeRunTask()
+        task.isEnabled = true
+        return task
+    }
+
+    override fun executeTask(context: DataContext, configuration: RunConfiguration, environment: ExecutionEnvironment, task: AzuriteBeforeRunTask): Boolean {
+        val project = configuration.project
+        val service = AzuriteService.getInstance()
+        if (!service.isRunning) {
+            service.start(project)
+        }
+        
+        return true
+    }
+}
