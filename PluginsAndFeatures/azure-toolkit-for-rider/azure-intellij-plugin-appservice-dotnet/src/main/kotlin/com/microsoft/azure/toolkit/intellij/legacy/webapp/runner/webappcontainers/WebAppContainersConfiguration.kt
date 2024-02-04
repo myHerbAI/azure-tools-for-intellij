85a26a93bd473a445edbde391b16168903633648
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-02-04 21:56:10 +0100

    Start refactoring WebAppContainersConfiguration

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/webapp/runner/webappcontainers/WebAppContainersConfiguration.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/webapp/runner/webappcontainers/WebAppContainersConfiguration.kt
@@ -8,6 +8,7 @@ package com.microsoft.azure.toolkit.intellij.legacy.webapp.runner.webappcontaine
 
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.LocatableConfigurationBase
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RuntimeConfigurationError
 import com.intellij.execution.runners.ExecutionEnvironment
@@ -18,122 +19,127 @@ import com.microsoft.azure.toolkit.intellij.legacy.utils.isValidResourceName
 import com.microsoft.azuretools.core.mvp.model.webapp.WebAppOnLinuxDeployModel
 
 class WebAppContainersConfiguration(private val project: Project, factory: ConfigurationFactory, name: String?) :
-    RiderAzureRunConfigurationBase<WebAppOnLinuxDeployModel>(project, factory, name) {
+        LocatableConfigurationBase<WebAppContainersConfigurationOptions>(project, factory, name) {
+
     companion object {
         private const val REPO_COMPONENT_REGEX_PATTERN = "[a-z0-9]+(?:[._-][a-z0-9]+)*"
         private const val TAG_REGEX_PATTERN = "^\\w+[\\w.-]*\$"
     }
 
-    private val webAppContainersModel = WebAppOnLinuxDeployModel()
+//    private val webAppContainersModel = WebAppOnLinuxDeployModel()
 
     private val repoComponentRegex = Regex(REPO_COMPONENT_REGEX_PATTERN)
     private val tagRegex = Regex(TAG_REGEX_PATTERN)
 
-    var webAppId: String?
-        get() = webAppContainersModel.webAppId
-        set(value) {
-            webAppContainersModel.webAppId = value
-        }
-    var webAppName: String
-        get() = webAppContainersModel.webAppName
-        set(value) {
-            webAppContainersModel.webAppName = value
-        }
-    var subscriptionId: String
-        get() = webAppContainersModel.subscriptionId
-        set(value) {
-            webAppContainersModel.subscriptionId = value
-        }
-    var locationName: String
-        get() = webAppContainersModel.locationName
-        set(value) {
-            webAppContainersModel.locationName = value
-        }
-    var resourceGroupName: String
-        get() = webAppContainersModel.resourceGroupName
-        set(value) {
-            webAppContainersModel.resourceGroupName = value
-        }
-    var pricingSkuTier: String?
-        get() = webAppContainersModel.pricingSkuTier
-        set(value) {
-            webAppContainersModel.pricingSkuTier = value
-        }
-    var pricingSkuSize: String
-        get() = webAppContainersModel.pricingSkuSize
-        set(value) {
-            webAppContainersModel.pricingSkuSize = value
-        }
-    var appServicePlanName: String?
-        get() = webAppContainersModel.appServicePlanName
-        set(value) {
-            webAppContainersModel.appServicePlanName = value
-        }
-    var appServicePlanResourceGroupName: String?
-        get() = webAppContainersModel.appServicePlanResourceGroupName
-        set(value) {
-            webAppContainersModel.appServicePlanResourceGroupName = value
-        }
-    var isCreatingNewWebAppOnLinux: Boolean
-        get() = webAppContainersModel.isCreatingNewWebAppOnLinux
-        set(value) {
-            webAppContainersModel.isCreatingNewWebAppOnLinux = value
-        }
-    var finalRepositoryName: String?
-        get() = webAppContainersModel.finalRepositoryName
-        set(value) {
-            webAppContainersModel.finalRepositoryName = value
-        }
-    var finalTagName: String?
-        get() = webAppContainersModel.finalTagName
-        set(value) {
-            webAppContainersModel.finalTagName = value
-        }
-    var port: Int
-        get() = webAppContainersModel.port
-        set(value) {
-            webAppContainersModel.port = value
-        }
+//    var webAppId: String?
+//        get() = webAppContainersModel.webAppId
+//        set(value) {
+//            webAppContainersModel.webAppId = value
+//        }
+//    var webAppName: String
+//        get() = webAppContainersModel.webAppName
+//        set(value) {
+//            webAppContainersModel.webAppName = value
+//        }
+//    var subscriptionId: String
+//        get() = webAppContainersModel.subscriptionId
+//        set(value) {
+//            webAppContainersModel.subscriptionId = value
+//        }
+//    var locationName: String
+//        get() = webAppContainersModel.locationName
+//        set(value) {
+//            webAppContainersModel.locationName = value
+//        }
+//    var resourceGroupName: String
+//        get() = webAppContainersModel.resourceGroupName
+//        set(value) {
+//            webAppContainersModel.resourceGroupName = value
+//        }
+//    var pricingSkuTier: String?
+//        get() = webAppContainersModel.pricingSkuTier
+//        set(value) {
+//            webAppContainersModel.pricingSkuTier = value
+//        }
+//    var pricingSkuSize: String
+//        get() = webAppContainersModel.pricingSkuSize
+//        set(value) {
+//            webAppContainersModel.pricingSkuSize = value
+//        }
+//    var appServicePlanName: String?
+//        get() = webAppContainersModel.appServicePlanName
+//        set(value) {
+//            webAppContainersModel.appServicePlanName = value
+//        }
+//    var appServicePlanResourceGroupName: String?
+//        get() = webAppContainersModel.appServicePlanResourceGroupName
+//        set(value) {
+//            webAppContainersModel.appServicePlanResourceGroupName = value
+//        }
+//    var isCreatingNewWebAppOnLinux: Boolean
+//        get() = webAppContainersModel.isCreatingNewWebAppOnLinux
+//        set(value) {
+//            webAppContainersModel.isCreatingNewWebAppOnLinux = value
+//        }
+//    var finalRepositoryName: String?
+//        get() = webAppContainersModel.finalRepositoryName
+//        set(value) {
+//            webAppContainersModel.finalRepositoryName = value
+//        }
+//    var finalTagName: String?
+//        get() = webAppContainersModel.finalTagName
+//        set(value) {
+//            webAppContainersModel.finalTagName = value
+//        }
+//    var port: Int
+//        get() = webAppContainersModel.port
+//        set(value) {
+//            webAppContainersModel.port = value
+//        }
+
+    override fun getState() =
+        options as? WebAppContainersConfigurationOptions
 
     override fun getState(executor: Executor, environment: ExecutionEnvironment) =
         WebAppContainersRunState(project, this)
 
-    override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> =
-        WebAppContainersSettingEditor(project)
-
-    override fun getModel() = webAppContainersModel
+    override fun getConfigurationEditor() = WebAppContainersSettingEditor(project)
 
     override fun checkConfiguration() {
-        checkAzurePreconditions()
-        validateDockerImageConfiguration()
-        with(webAppContainersModel) {
-            if (isCreatingNewWebAppOnLinux) {
-                if (webAppName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a Web App for Containers")
-                if (!isValidResourceName(webAppName)) throw RuntimeConfigurationError("Web App names only allow alphanumeric characters and hyphens, cannot start or end in a hyphen, and must be less than 60 chars")
-                if (subscriptionId.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a Subscription")
-                if (resourceGroupName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a Resource Group")
-                if (!isValidResourceName(resourceGroupName)) throw RuntimeConfigurationError("Resource group names only allow alphanumeric characters and hyphens, cannot start or end in a hyphen, and must be less than 60 chars")
-                if (appServicePlanName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify an App Service plan")
-                if (appServicePlanName.isNullOrEmpty()) throw RuntimeConfigurationError("App Service plan name is not provided")
-            } else {
-                if (webAppId.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify Web App for Containers")
-            }
+        val options = getState() ?: return
+        with(options) {
+
         }
+//        checkAzurePreconditions()
+//        validateDockerImageConfiguration()
+//        with(webAppContainersModel) {
+//            if (isCreatingNewWebAppOnLinux) {
+//                if (webAppName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a Web App for Containers")
+//                if (!isValidResourceName(webAppName)) throw RuntimeConfigurationError("Web App names only allow alphanumeric characters and hyphens, cannot start or end in a hyphen, and must be less than 60 chars")
+//                if (subscriptionId.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a Subscription")
+//                if (resourceGroupName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a Resource Group")
+//                if (!isValidResourceName(resourceGroupName)) throw RuntimeConfigurationError("Resource group names only allow alphanumeric characters and hyphens, cannot start or end in a hyphen, and must be less than 60 chars")
+//                if (appServicePlanName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify an App Service plan")
+//                if (appServicePlanName.isNullOrEmpty()) throw RuntimeConfigurationError("App Service plan name is not provided")
+//            } else {
+//                if (webAppId.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify Web App for Containers")
+//            }
+//        }
     }
 
-    private fun validateDockerImageConfiguration() {
-        val repositoryName = finalRepositoryName
-        val tagName = finalTagName
-        if (repositoryName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a container registry and an image.")
-        if (tagName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify an image tag.")
-        if (repositoryName.length > 255) throw RuntimeConfigurationError("The length of repository name must be less than 256 characters.")
-        val repositoryParts = repositoryName.split('/')
-        if (repositoryParts.last().isEmpty()) throw RuntimeConfigurationError("Please specify an image name.")
-        if (repositoryName.endsWith('/')) throw RuntimeConfigurationError("The repository name should not end with '/'.")
-        repositoryParts.forEach {
-            if (!repoComponentRegex.matches(it)) throw RuntimeConfigurationError("Invalid repository component: $it, should follow: $REPO_COMPONENT_REGEX_PATTERN")
-        }
-        if (tagName.length > 127) throw RuntimeConfigurationError("The length of tag name must be less than 128 characters")
-        if (!tagRegex.matches(tagName)) throw RuntimeConfigurationError("Invalid tag: $tagName, should follow: $TAG_REGEX_PATTERN")
-    }
+//    private fun validateDockerImageConfiguration() {
+//        val repositoryName = finalRepositoryName
+//        val tagName = finalTagName
+//        if (repositoryName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify a container registry and an image.")
+//        if (tagName.isNullOrEmpty()) throw RuntimeConfigurationError("Please specify an image tag.")
+//        if (repositoryName.length > 255) throw RuntimeConfigurationError("The length of repository name must be less than 256 characters.")
+//        val repositoryParts = repositoryName.split('/')
+//        if (repositoryParts.last().isEmpty()) throw RuntimeConfigurationError("Please specify an image name.")
+//        if (repositoryName.endsWith('/')) throw RuntimeConfigurationError("The repository name should not end with '/'.")
+//        repositoryParts.forEach {
+//            if (!repoComponentRegex.matches(it)) throw RuntimeConfigurationError("Invalid repository component: $it, should follow: $REPO_COMPONENT_REGEX_PATTERN")
+//        }
+//        if (tagName.length > 127) throw RuntimeConfigurationError("The length of tag name must be less than 128 characters")
+//        if (!tagRegex.matches(tagName)) throw RuntimeConfigurationError("Invalid tag: $tagName, should follow: $TAG_REGEX_PATTERN")
+//    }
 }
--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/webapp/runner/webappcontainers/WebAppContainersSettingEditor.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/webapp/runner/webappcontainers/WebAppContainersSettingEditor.kt
@@ -4,16 +4,101 @@
 
 package com.microsoft.azure.toolkit.intellij.legacy.webapp.runner.webappcontainers
 
+import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.microsoft.azure.toolkit.intellij.legacy.common.RiderAzureSettingsEditor
+import com.intellij.ui.JBIntSpinner
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.dsl.builder.*
+import com.microsoft.azure.toolkit.intellij.common.AzureContainerRegistryComboBox
+import com.microsoft.azure.toolkit.intellij.common.ContainerRegistryModel
+import com.microsoft.azure.toolkit.intellij.common.dockerContainerRegistryComboBox
+import com.microsoft.azure.toolkit.lib.appservice.config.AppServicePlanConfig
+import com.microsoft.azure.toolkit.lib.appservice.model.OperatingSystem
+import com.microsoft.azure.toolkit.lib.appservice.model.PricingTier
+import com.microsoft.azure.toolkit.lib.common.model.Region
+import com.microsoft.azure.toolkit.lib.common.model.Subscription
+import com.microsoft.azure.toolkit.lib.resource.ResourceGroupConfig
+import javax.swing.JLabel
+import javax.swing.JPanel
 
-class WebAppContainersSettingEditor(project: Project) : RiderAzureSettingsEditor<WebAppContainersConfiguration>() {
-    private val panel = WebAppContainersSettingPanel(project)
+class WebAppContainersSettingEditor(private val project: Project) :
+        SettingsEditor<WebAppContainersConfiguration>() {
+
+    private val panel: JPanel
+    private lateinit var containerRegistryComboBox: Cell<AzureContainerRegistryComboBox>
+    private lateinit var repositoryLabel: Cell<JLabel>
+    private lateinit var repositoryTextField: Cell<JBTextField>
+    private lateinit var tagTextField: Cell<JBTextField>
+    private lateinit var webAppContainersComboBox: Cell<WebAppContainersComboBox>
+    private lateinit var portSpinner: Cell<JBIntSpinner>
 
     init {
-        Disposer.register(this, panel)
+        panel = panel {
+            row("Container Registry:") {
+                containerRegistryComboBox = dockerContainerRegistryComboBox()
+                    .align(Align.FILL)
+                    .resizableColumn()
+            }
+            row("Repository:") {
+                repositoryLabel = label("")
+                repositoryTextField = textField()
+                label("Tag:")
+                tagTextField = textField()
+                    .columns(COLUMNS_TINY)
+            }
+            row("Web App:") {
+                webAppContainersComboBox = dockerWebAppComboBox(project)
+                    .align(Align.FILL)
+                Disposer.register(this@WebAppContainersSettingEditor, webAppContainersComboBox.component)
+            }
+            row("Website Port:") {
+                portSpinner = spinner(80..65535)
+            }
+        }
+
+        tagTextField.component.text = "latest"
+        containerRegistryComboBox.component.addValueChangedListener(::onRegistryChanged)
+        webAppContainersComboBox.component.reloadItems()
     }
 
-    override fun getPanel() = panel
+    private fun onRegistryChanged(value: ContainerRegistryModel) {
+        repositoryLabel.component.text = "${value.address}/"
+    }
+
+    override fun resetEditorFrom(configuration: WebAppContainersConfiguration) {
+        val state = configuration.state ?: return
+
+        if (state.resourceId.isNullOrEmpty() && state.webAppName.isNullOrEmpty()) return
+
+        val subscription = Subscription(requireNotNull(state.subscriptionId))
+        val region = if (state.region.isNullOrEmpty()) null else Region.fromName(requireNotNull(state.region))
+        val resourceGroupName = state.resourceGroupName
+        val resourceGroup = ResourceGroupConfig
+            .builder()
+            .subscriptionId(subscription.id)
+            .name(resourceGroupName)
+            .region(region)
+            .build()
+        val pricingTier = PricingTier(state.pricingTier, state.pricingSize)
+        val plan = AppServicePlanConfig
+            .builder()
+            .subscriptionId(subscription.id)
+            .name(state.appServicePlanName)
+            .resourceGroupName(resourceGroupName)
+            .region(region)
+            .os(OperatingSystem.LINUX)
+            .pricingTier(pricingTier)
+            .build()
+    }
+
+    override fun applyEditorTo(configuration: WebAppContainersConfiguration) {
+        val state = configuration.state ?: return
+
+        state.apply {
+
+        }
+    }
+
+    override fun createEditor() = panel
 }
