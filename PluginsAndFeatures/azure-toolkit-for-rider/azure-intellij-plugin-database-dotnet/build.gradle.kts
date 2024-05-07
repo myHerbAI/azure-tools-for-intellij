b1db5a968b19d7b25b71086d83adedd6970f88ef
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-05-07 09:03:03 +0200

    Add a azure-intellij-plugin-database library to the plugin dependencies

---
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-database-dotnet/build.gradle.kts
@@ -0,0 +1,12 @@
+dependencies {
+    compileOnly(project(path = ":azure-intellij-plugin-lib"))
+    runtimeOnly(project(path = ":azure-intellij-plugin-lib", configuration = "instrumentedJar"))
+    compileOnly(project(path = ":azure-intellij-plugin-database"))
+    runtimeOnly(project(path = ":azure-intellij-plugin-database", configuration = "instrumentedJar"))
+    compileOnly(project(path = ":azure-intellij-resource-connector-lib"))
+    runtimeOnly(project(path = ":azure-intellij-resource-connector-lib", configuration = "instrumentedJar"))
+    implementation("com.microsoft.azure:azure-toolkit-database-lib")
+    implementation("com.microsoft.azure:azure-toolkit-mysql-lib")
+    implementation("com.microsoft.azure:azure-toolkit-sqlserver-lib")
+    implementation("com.microsoft.azure:azure-toolkit-postgre-lib")
+}
