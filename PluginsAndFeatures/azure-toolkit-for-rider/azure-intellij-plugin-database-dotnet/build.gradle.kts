680c84f3b2437e2e0580447682a6b40611bdf62e
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-05-08 13:07:07 +0200

    Add database specific extensions

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-database-dotnet/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-database-dotnet/build.gradle.kts
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2018-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the MIT license.
+ */
+
+intellij {
+    plugins = listOf("com.intellij.database")
+}
+
 dependencies {
     compileOnly(project(path = ":azure-intellij-plugin-lib"))
     runtimeOnly(project(path = ":azure-intellij-plugin-lib", configuration = "instrumentedJar"))
