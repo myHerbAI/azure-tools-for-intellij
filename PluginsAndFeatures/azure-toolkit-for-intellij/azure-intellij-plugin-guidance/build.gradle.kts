86c657804350bd7e44cf2b21bd27578ddbe29ce3
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-06-19 16:01:56 +0200

    Updates

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-guidance/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-guidance/build.gradle.kts
@@ -63,10 +63,11 @@ tasks {
     }
 
     buildPlugin { enabled = false }
-    runIde { enabled = false }
-    prepareSandbox { enabled = false }
-    buildSearchableOptions { enabled = false }
     patchPluginXml { enabled = false }
+    prepareSandbox { enabled = false }
     publishPlugin { enabled = false }
+    runIde { enabled = false }
+    signPlugin { enabled = false }
+    buildSearchableOptions { enabled = false }
     verifyPlugin { enabled = false }
 }
--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/build.gradle.kts
@@ -40,10 +40,11 @@ dependencies {
 
 tasks {
     buildPlugin { enabled = false }
-    runIde { enabled = false }
-    prepareSandbox { enabled = false }
-    buildSearchableOptions { enabled = false }
     patchPluginXml { enabled = false }
+    prepareSandbox { enabled = false }
     publishPlugin { enabled = false }
+    runIde { enabled = false }
+    signPlugin { enabled = false }
+    buildSearchableOptions { enabled = false }
     verifyPlugin { enabled = false }
 }
