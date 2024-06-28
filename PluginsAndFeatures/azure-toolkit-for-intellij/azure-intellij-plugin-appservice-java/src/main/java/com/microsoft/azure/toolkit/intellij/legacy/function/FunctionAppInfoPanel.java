438bf4c77802883c2946e36edb3e9c7cd63a6f73
Flanker32 <hanli@microsoft.com>
2024-06-28 16:55:35 +0800

    Fix could not select an existing container apps environment during function creation

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-appservice-java/src/main/java/com/microsoft/azure/toolkit/intellij/legacy/function/FunctionAppInfoPanel.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-appservice-java/src/main/java/com/microsoft/azure/toolkit/intellij/legacy/function/FunctionAppInfoPanel.java
@@ -141,7 +141,7 @@ public class FunctionAppInfoPanel extends JPanel implements AzureFormPanel<Funct
         } else if (rdoContainerAppsEnvironment.isSelected()) {
             config.diagnosticConfig(null);
             Optional.ofNullable(cbEnvironment.getValue()).ifPresent(e -> {
-                if (e instanceof ContainerAppsEnvironmentDraft draft) {
+                if (e instanceof ContainerAppsEnvironmentDraft draft && draft.isDraftForCreating()) {
                     config.environmentConfig(draft.getConfig());
                 } else {
                     config.environment(e.getName());
