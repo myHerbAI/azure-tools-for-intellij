c27ff5c93dbf3476be7476a1f4f14b24d799c682
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-09-03 13:11:53 +0200

    Set platform plugins inside build.gradle.kts

--- PluginsAndFeatures/azure-toolkit-for-rider/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-rider/build.gradle.kts
@@ -75,15 +75,8 @@ dependencies {
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         rider(platformVersion, false)
-
         jetbrainsRuntime()
-
-        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
-        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
-
-        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
-        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
-
+        bundledPlugins(listOf("com.jetbrains.restClient"))
         instrumentationTools()
         pluginVerifier()
         zipSigner()
